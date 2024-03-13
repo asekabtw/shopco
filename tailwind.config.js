913b0a354d7034ef982d4dc9955a73588da00296
Assylzhan <asekabtw@gmail.com>
2024-03-13 16:09:06 +0100

    add top selling and new arrivals with scroll mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
@@ -8,5 +10,5 @@ module.exports = {
       second: '"Plus Jakarta Sans", sans-serif',
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
