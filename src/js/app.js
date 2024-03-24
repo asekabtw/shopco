c1f27c7949b8e13e0ed7fd68ee1bcc9f6385de24
Assylzhan <asekabtw@gmail.com>
2024-03-24 17:58:32 +0100

    add burger button and menu mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((json) => console.log(json));
+
+const burger = document.getElementById("burger");
+const burgerX = document.getAnimations("burgerX");
+const body = document.getElementById("body");
+const menu = document.getElementById("menu");
+
+burger.addEventListener("click", (btn) => {
+  menu.classList.toggle("hidden");
+});
