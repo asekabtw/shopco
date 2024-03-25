1e48cba91b004b72e35741b5b3620fe5359701af
Assylzhan <asekabtw@gmail.com>
2024-03-25 21:55:16 +0100

    basics of searchBar mobile

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,16 @@ fetch("https://fakestoreapi.com/products")
   .then((json) => console.log(json));
 
 const burger = document.getElementById("burger");
-const burgerX = document.getAnimations("burgerX");
+const searchBtn = document.getElementById("searchBtn");
+const searchBar = document.getElementById("searchBar");
 const body = document.getElementById("body");
 const menu = document.getElementById("menu");
 
 burger.addEventListener("click", (btn) => {
   menu.classList.toggle("show");
   body.classList.toggle("overflow-hidden");
+});
+
+searchBtn.addEventListener("click", (btn) => {
+  searchBar.classList.remove("hidden");
 });
